f5b48bf9ea8f0e576ed24d36652b0bf9f75ad616
Tim D'hoe <timd93@gmail.com>
2012-08-06 13:48:59 +0200

    First Commit
    First fully working version

---
+++ src/com/timdhoe/tool/sms_enabler/FileUpload.java
@@ -0,0 +1,89 @@
+package com.timdhoe.tool.sms_enabler;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.util.Log;
+
+public class FileUpload extends Thread implements Runnable {
+	private File f;
+	private String url;
+	public String fileMD5;
+	
+	public FileUpload(File f, String url, String fileMD5) {
+		this.url = url;
+		this.f = f;
+		this.fileMD5 = fileMD5;
+		// initializing and starting a new local Thread object
+        Thread uploadThread = new Thread(this);
+        uploadThread.start();
+	}
+	
+	@Override
+	public void run() {
+		upload();
+	}
+	
+	private void upload() {
+		HttpClient httpclient = new DefaultHttpClient();
+		HttpPost httppost = new HttpPost(url);
+		FileBody file = new FileBody(f);
+		try {
+			MultipartEntity entity = new MultipartEntity();
+			entity.addPart("file", file);
+			httppost.setEntity(entity);
+			HttpResponse response = httpclient.execute(httppost);
+			//Check Status Code
+			Log.i("SMS-Enabler: HTTP-Status", String.valueOf(response.getStatusLine().getStatusCode()));
+			Log.i("SMS-Enabler: HTTP-Post", String.valueOf(httppost.getRequestLine()));
+			//Send process request
+			URL url = new URL("http://ec2-50-112-53-255.us-west-2.compute.amazonaws.com:8080/process.php?md5="+fileMD5);
+			Log.i("SMS-Enabler","PROCESS " + url.toString());
+			URLConnection conn= url.openConnection();
+			InputStream in = new BufferedInputStream(conn.getInputStream());
+			try {
+				readStream(in);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} finally {
+			     in.close();
+			}
+			Log.e("APPP","Connected");
+		} catch (ClientProtocolException e) {
+			Log.e("SMS-ENABLER","Client Protocol Exception");
+			e.printStackTrace();
+		} catch (IOException e) {
+			Log.i("SMS-ENABLER","PROCESS invoked");
+		} 
+		new FileRetrieve(fileMD5);
+
+	}
+	
+	private String readStream(InputStream is) {
+	    try {
+	      ByteArrayOutputStream bo = new ByteArrayOutputStream();
+	      int i = is.read();
+	      while(i != -1) {
+	        bo.write(i);
+	        i = is.read();
+	      }
+	      return bo.toString();
+	    } catch (IOException e) {
+	      return "";
+	    }
+	}
+}
---
+++ src/com/timdhoe/tool/sms_enabler/IntegrityUtils.java
@@ -0,0 +1,49 @@
+package com.timdhoe.tool.sms_enabler;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.security.MessageDigest;
+
+public class IntegrityUtils {
+public static String toASCII(byte b[], int start, int length) {
+    StringBuffer asciiString = new StringBuffer();
+
+    for (int i = start; i < (length + start); i++) {
+        // exclude nulls from the ASCII representation
+        if (b[i] != (byte) 0x00) {
+            asciiString.append((char) b[i]);
+        }
+    }
+
+    return asciiString.toString();
+}
+
+public static String getMD5Checksum(File file) throws Exception {
+    byte[] b = createChecksum(file);
+    String result = "";
+
+    for (int i = 0; i < b.length; i++) {
+        result += Integer.toString((b[i] & 0xff) + 0x100, 16).substring(1);
+    }
+    return result;
+}
+
+public static byte[] createChecksum(File file) throws Exception {
+    InputStream fis = new FileInputStream(file);
+
+    byte[] buffer = new byte[1024];
+    MessageDigest complete = MessageDigest.getInstance("MD5");
+    int numRead;
+
+    do {
+        numRead = fis.read(buffer);
+        if (numRead > 0) {
+            complete.update(buffer, 0, numRead);
+        }
+    } while (numRead != -1);
+
+    fis.close();
+    return complete.digest();
+}
+}
